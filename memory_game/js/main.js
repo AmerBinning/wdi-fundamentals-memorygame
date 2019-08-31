ece2ea0e2cd124d455e2cc74c89775f47a7acab1
Amer <amerbinning@hotmail.com>
2019-08-31 18:25:02 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,30 @@
+console.log("Up and running!");
+
+var cards = [
+	{
+		suitRank: "heartQueen",
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "memory_game/images/queen-of-hearts.png"
+	},
+	{
+		suitRank: "diamondQueen",
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "memory_game/images/queen-of-diamonds.png"
+	},
+	{
+		suitRank: "heartKing",
+		rank: "king",
+		suit: "hearts",
+		cardImage: "memory_game/images/king-of-hearts.png"
+	},
+	{
+		suitRank: "diamondKing",
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "memory_game/images/king-of-diamonds.png"
+	}
+
+];
+
